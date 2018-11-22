0d8bf6c32b309d9365e82172441d6ff8a791f6ca
SDM123 <s.wells@napier.ac.uk>
2018-11-22 15:17:23 +0000

    Working on tags

--- Sourcecode/coursework.py
+++ Sourcecode/coursework.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def root():
    return render_template('Home.html'), 200
 
+@app.route('/Tags')
+def tags():
+   return render_template('Tags.html'), 200
+
+
 if __name__ == "__main__":
    app.run(host='0.0.0.0', debug=True)
