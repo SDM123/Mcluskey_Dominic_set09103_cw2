40ea960dab3069424ada72cd56afc0423c2c7a60
SDM123 <40277061@live.napier.ac.uk>
2018-11-22 15:47:57 +0000

    fixed the problem

--- Sourcecode/coursework.py
+++ Sourcecode/coursework.py
@@ -9,6 +9,12 @@ def root():
 def tags():
    return render_template('Tags.html'), 200
 
+@app.route('/Tags/Appearance')
+def appearance():
+   return render_template('Appearance.html'), 200
+
+
+
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', debug=True)
